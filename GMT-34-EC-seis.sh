34e1413a8317fc2516e65cb255292bf3e78343c3
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-07-13 10:03:16 +0300

    Add files via upload

--- GMT-34-EC-seis.sh
+++ GMT-34-EC-seis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Purpose: shaded relief grid raster map from the GEBCO 15 arc sec global data set (here: Uganda)
+# Purpose: shaded relief grid raster map from the GEBCO 15 arc sec global data set (here: Equador)
 # GMT modules: gmtset, gmtdefaults, grdcut, makecpt, grdimage, psscale, grdcontour, psbasemap, gmtlogo, psconvert
-# http://soliton.vm.bytemark.co.uk/pub/cpt-city/lb/mp/index.html
+# http://soliton.vm.bytemark.co.uk/pub/cpt-city/rf/index.html
 
 # GMT set up
 gmt set FORMAT_GEO_MAP=dddF \
