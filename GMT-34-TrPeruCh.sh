8f6012874f0acfdd610f7a7d4c64055883ece601
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-11-21 19:35:40 +0300

    Add files via upload

--- GMT-34-TrPeruCh.sh
+++ GMT-34-TrPeruCh.sh
@@ -18,36 +18,43 @@ gmt set FORMAT_GEO_MAP=dddF \
 # Overwrite defaults of GMT
 gmtdefaults -D > .gmtdefaults
 
+exec bash
+
+cd /Users/polinalemenkova/Downloads/
+
+tr ',' '\t' < IEB_export.csv > IEB_export.tsv
+
 # Extract a subset of ETOPO1m for the Iceland area
-gmt grdcut ETOPO1_Ice_g_gmt4.grd -R42/55/10/20 -Gye_relief1.nc
-gmt grdcut GEBCO_2019.nc -R42/55/10/20 -Gye_relief.nc
-gdalinfo -stats ye_relief.nc
-# Minimum=-3549.000, Maximum=7966.000
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R276/292/-30/-6 -Gpct_relief1.nc
+gmt grdcut GEBCO_2023.nc -R276/292/-30/-6 -Gpct_relief.nc
+gmt grdinfo pct_relief.nc
+# v_min: -8997 v_max: 6781 name
 
 # Make color palettes
 # for topography
 #gmt makecpt -Ctopo.cpt -V -T-5358/3447 > myocean.cpt
 #gmt makecpt -Ceurope_3.cpt -V -T-5358/3447 > myocean.cpt
-gmt makecpt -Cmoon.cpt -V -T-5358/3447 > myocean.cpt
+gmt makecpt -Cgmt/gray -V -T-8997/6781 > myocean.cpt
 # for earthquakes
 #gmt makecpt -Cred,green,blue -T0,7,9,12 > quakes.cpt
 #gmt makecpt -Cseis > quakes.cpt
-gmt makecpt -Cseis -T4/6/0.1 -Z > steps.cpt
+#gmt makecpt -Cseis -T4/6/0.1 -Z > steps.cpt
+gmt makecpt -Cseis -T4/6.0/0.1 -Z > steps.cpt
 
-ps=Geol_YE.ps
+ps=Seis_PCT.ps
 # Make raster image
-gmt grdimage ye_relief.nc -Cmyocean.cpt -R42/55/10/20 -JM6.5i -I+a15+ne0.75 -Xc -K > $ps
+gmt grdimage pct_relief1.nc -Cmyocean.cpt -R276/292/-30/-6 -JM6.5i -I+a15+ne0.75 -Xc -K > $ps
 
 # Add legend
 gmt psscale -Dg40.5/10.0+w13.0c/0.15i+v+o0.3/0i+ml -R -J -Cmyocean.cpt \
 	--FONT_LABEL=7p,Helvetica,black \
     --FONT_ANNOT_PRIMARY=7p,Helvetica,black \
     --FONT_TITLE=6p,Helvetica,black \
-	-Bg500f50a500+l"Color ramp: 'moon' by Paul Naylor for the Ordnance Survey, UK [R=-5358/3447, continuous, RGB, 46 segments]" \
+	-Bg500f50a500+l"Color ramp: grey [R=-8997/6781, continuous, RGB, 46 segments]" \
 	-I0.2 -By+lm -O -K >> $ps
     
 # Add shorelines
-gmt grdcontour ye_relief1.nc -R -J -C1000 -W0.1p -O -K >> $ps
+gmt grdcontour pct_relief1.nc -R -J -C1000 -W0.1p -O -K >> $ps
 
 # Add coastlines, borders, rivers
 gmt pscoast -R -J -P \
@@ -61,7 +68,7 @@ gmt psbasemap -R -J \
     --FONT_ANNOT_PRIMARY=7p,Helvetica,black \
     --FONT_LABEL=7p,Helvetica,black \
     -Bpxg2f1a2 -Bpyg2f1a2 -Bsxg2 -Bsyg1 \
-    -B+t"Seismic and tectonic setting in Yemen and Gulf of Aden" -O -K >> $ps
+    -B+t"Seismic setting in Peru-Chile Trench" -O -K >> $ps
     
 # Add scale, directional rose
 gmt psbasemap -R -J \
@@ -75,166 +82,10 @@ gmt psbasemap -R -J \
     -UBL/-5p/-105p -O -K >> $ps
 
 # Texts
-gmt pstext -R -J -N -O -K \
--F+jTL+f10p,25,red+jLB+a-340 >> $ps << EOF
-53.0 17.8 O    M    A    N
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f10p,25,red+jLB+a-340 >> $ps << EOF
-46.1 17.8 S  A  U  D  I     A  R  A  B  I  A
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f8p,25,red+jLB+a-330 -Gwhite@30 >> $ps << EOF
-42.1 11.5 DJIBOUTI
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f10p,25,red+jLB+a-350 >> $ps << EOF
-47.0 10.3 S   O   M   A   L   I
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f16p,25,darkbrown+jLB+a-340 >> $ps << EOF
-46.3 15.9 Y        E        M        E        N
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f8p,25,red+jLB+a-50 -Gwhite@30 >> $ps << EOF
-42.0 13.4 ERITREA
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f7p,25,red+jLB+a-60 -Gwhite@30 >> $ps << EOF
-42.0 10.9 ETHIOPIA
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f12p,13,blue+jLB+a-340 -Gwhite@50 >> $ps << EOF
-46.5 11.7 G u l f   o f   A d e n
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f11p,13,white+jLB >> $ps << EOF
-53.4 15.8 Arabian
-53.7 15.4 Sea
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f11p,13,white+jLB >> $ps << EOF
-53.7 10.8 Indian
-53.6 10.4 Ocean
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f9p,26,blue2+jLB+a-60 -Gwhite@70 >> $ps << EOF
-42.9 13.4 Bab-el-Mandeb
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f9p,26,chocolate4+jLB+a-340 >> $ps << EOF
-46.1 18.7 A R   R U B'  A L  K H A L I
-46.5 18.5 (Empty Quarter Desert)
-EOF
-gmt pstext -R -J -N -O -K \
--F+jTL+f10p,26,blue2+jLB+a-82 -Gwhite@70 >> $ps << EOF
-42.2 16.5 R e d   S e a
-EOF
-gmt pstext -R -J -N -O -K \
--F+f11p,13,brown4+jLB >> $ps << EOF
-53.5 12.0 Socotra
-EOF
-gmt pstext -R -J -N -O -K \
--F+f13p,13,darkbrown+jLB+a-345 >> $ps << EOF
-47.7 15.1 H A D H R A M A U T
-EOF
-gmt pstext -R -J -N -O -K \
--F+f11p,13,darkbrown+jLB+a-18 >> $ps << EOF
-51.0 17.1 Jabal Mahra
-EOF
-gmt pstext -R -J -N -O -K \
--F+f10p,13,darkbrown+jLB >> $ps << EOF
-50.1 16.8 Jabal Bin
-50.1 16.5 Kushayt
-EOF
-gmt pstext -R -J -N -O -K \
--F+f10p,13,darkbrown+jLB+a-45 >> $ps << EOF
-44.6 17.3 Ramlat Dahm
-EOF
-gmt pstext -R -J -N -O -K \
--F+f10p,13,darkbrown+jLB+a-330 >> $ps << EOF
-46.4 15.3 Ramlat
-46.4 15.0 al-Sab'atayn
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,darkbrown+jLB >> $ps << EOF
-43.3 15.2 Jabal
-43.3 14.9 Haraz
-EOF
-# cities
-gmt pstext -R -J -N -O -K \
--F+f11p,13,black+jLB -Gwhite@30 >> $ps << EOF
-44.2 15.4 Sana'a
-EOF
-gmt psxy -R -J -Ss -W0.5p -Gred -O -K << EOF >> $ps
-44.1 15.2 0.40c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f10p,13,black+jLB -Gwhite@30 >> $ps << EOF
-45.1 13.1 Aden
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-45.0 13.0 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-44.2 13.2 Taiz
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-44.0 13.3 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-43.1 14.6 Al Hudaydah
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-43.0 14.5 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-44.2 13.7 Ibb
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-44.1 13.6 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f10p,13,black+jLB -Gwhite@30 >> $ps << EOF
-49.1 14.4 Al Mukalla
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-49.0 14.3 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-44.3 14.4 Dhamar
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-44.2 14.3 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-43.3 15.6 Amran
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-43.6 15.4 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-43.5 16.7 Sadah
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-43.4 16.6 0.20c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f10p,13,black+jLB -Gwhite@30 >> $ps << EOF
-51.5 15.8 Al Ghaydah
-EOF
-gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
-52.1 16.1 0.20c
-EOF
 
 # Add earthquake points
-gmt psxy -R -J quakes_Yemen.ngdc -Wfaint -i4,3,6,6s0.1 -h3 -Scc -Csteps.cpt -O -K >> $ps
+#gmt psxy -R -J quakes_PCT.csv -Wfaint -i4,3,6,6s0.1 -h3 -Scc -Csteps.cpt -O -K >> $ps
+gmt psxy -R -J IEB_export.tsv -Wfaint -i4,3,6,6+s0.1 -h3 -Scc -Csteps.cpt -O -K >> $ps
 
 # Add geological lines and points
 gmt psxy -R -J volcanoes.gmt -St0.4c -Gred -Wthinnest -O -K >> $ps
@@ -243,12 +94,12 @@ gmt psxy -R -J volcanoes.gmt -St0.4c -Gred -Wthinnest -O -K >> $ps
 gmt psxy -R -J GSFML_SF_FZ_KM.gmt -Wthicker,goldenrod1 -O -K >> $ps
 gmt psxy -R -J GSFML_SF_FZ_RM.gmt -Wthicker,pink -O -K >> $ps
 gmt psxy -R -J LIPS.2011.gmt -L -Gpink1@50 -Wthinnest,red -O -K >> $ps
-gmt psxy -R -J ophiolites.gmt -Sc0.15c -Gmagenta -Wthinnest -O -K >> $ps
+#gmt psxy -R -J ophiolites.gmt -Sc0.15c -Gmagenta -Wthinnest -O -K >> $ps
 gmt psxy -R -J ridge.gmt -Sf0.5c/0.15c+l+t -Wthin,red -Gyellow -O -K >> $ps
 gmt psxy -R -J ridge.gmt -Sc0.05c -Gred -Wthickest,red -O -K >> $ps
 # tectonic plates
-gmt psxy -R -J TP_Arabian.txt -L -Wthicker,purple -O -K >> $ps
-gmt psxy -R -J TP_African.txt -L -Wthickest,purple -O -K >> $ps
+#gmt psxy -R -J TP_Arabian.txt -L -Wthicker,purple -O -K >> $ps
+#gmt psxy -R -J TP_African.txt -L -Wthickest,purple -O -K >> $ps
 #
 gmt psmeca -R CMT.txt -J -Sd0.4/2/u -Gred -L0.1p -O -K >> $ps
 gmt psmeca -R CMT.txt -J -Sc0.1/2/u -Gred -L0.1p -Fa/5p/it \
@@ -258,14 +109,10 @@ gmt psmeca CMT.txt -R -J -Sd0.5/2/u -Gred -L0.1p -Fa/5p/it \
     -W0.1p -Fz -Ewhite -O -K >> $ps
 
 #Texts
-gmt pstext -R -J -N -O -K \
--F+f12p,Helvetica,brown+jLB >> $ps << EOF
-45.0 17.3 A R A B I A N  P L A T E
-EOF
-gmt pstext -R -J -N -O -K \
--F+f12p,Helvetica,brown+jLB -Gwhite@60 >> $ps << EOF
-44.2 10.8 A F R I C A N   P L A T E
-EOF
+#gmt pstext -R -J -N -O -K \
+#-F+f12p,Helvetica,brown+jLB -Gwhite@60 >> $ps << EOF
+#44.2 10.8 A F R I C A N   P L A T E
+#EOF
 
 # Add legend -3.0
 gmt pslegend -R -J -Dx0.3/-2.0+w18.0c+o-1.5/0.1c \
@@ -299,8 +146,8 @@ gmt logo -Dx7.0/-4.5+o0.1i/0.1i+w2c -O -K >> $ps
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y8.0c -N -O \
     -F+f11p,13,black+jLB >> $ps << EOF
-1.2 8.9 Digital relief model: GEBCO/SRTM 15 arc-second grid of Earth's surface
+1.2 8.9 Digital relief model: GEBCO/SRTM 15 arc-second grid of Earth surface
 EOF
 
 # Convert to image file using GhostScript
-gmt psconvert Geol_YE.ps -A2.5c -E720 -Tj -Z
+gmt psconvert Seis_PCT.ps -A2.5c -E720 -Tj -Z
